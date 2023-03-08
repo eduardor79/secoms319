84d8cee249f5e7e6a5490c8d94438369f062b89b
eduardor79 <eduardo.ram79@gmail.com>
2023-03-08 12:41:53 -0600

    read from data.json

--- ssh.py
+++ ssh.py
@@ -1,5 +1,6 @@
 from paramiko import SSHClient, AutoAddPolicy
 import json
+import time
 
 client = SSHClient()
 client.load_system_host_keys()
@@ -10,9 +11,11 @@ client.connect('192.168.1.13', username='pi', password='raspberry')
 # insert command for running script
 while (1):
     stdin, stdout, stderr = client.exec_command("python3 DHT11.py")
+    stdin, stdout, stderr = client.exec_command("cat data.json")
     print(stdout.read().decode())
     with open('data.json', 'w') as f:
         json.dump(stdout.read().decode(), f)
+    time.sleep(2)
 
 # close everything
 stdin.close()
