35e3a6d11a1610e5d812f2a471143c36fa32b8ec
eduardor79 <eduardo.ram79@gmail.com>
2023-03-07 22:35:13 -0600

    Add Temp and Humidity js
    put the data.json information into each page

--- humidity.js
+++ humidity.js
@@ -4,15 +4,10 @@ fetch("data.json")
     .then(data => dataToHTML(data))
 
 function dataToHTML(data){
-    string1 = data.Temperature+" degrees F"
-    string2 = data.TemperatureinC+" degrees C"
+    string1 = data.Humidity+"%"
     var p1 = document.createElement("p")
-    var p2 = document.createElement("p")
     p1.innerHTML = string1
-    p2.innerHTML = string2
     var div1 = document.getElementById("mainDiv")
-    var div2 = document.getElementById("mainDiv")
     div1.appendChild(p1)
-    div2.appendChild(p2)
 
 }
